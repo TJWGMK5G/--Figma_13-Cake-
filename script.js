fbe20c3f8d69759d5f6cbbb15e35a6cbe29b7397
TJWGMK5G <87062165+TJWGMK5G@users.noreply.github.com>
2022-08-13 19:31:33 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function () {
+//   $(".main-slider-content").slick();
+// });
+
+let openBtn = document.querySelector("#main-catalog-item-ordersummerid");
+openBtn.addEventListener("click", function () {
+  openBtn.classList.add("main-catalog-item-ordersummer");
+  openBtn.style.color = "#fff";
+  openBtn.textContent = "Нет в наличии!";
+});
